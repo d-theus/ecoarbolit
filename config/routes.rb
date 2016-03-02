2cb0de0e69cbabac8c8ce62dfca8b94418b4f841
d-theus <slma0x02@gmail.com>
2016-03-02 11:58:54 +0300

    pros layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :pros, only: [] do
+    get :level,           on: :collection
+    get :insulating,      on: :collection
+    get :solid,           on: :collection
+    get :waterproof,      on: :collection
+    get :eco,             on: :collection
+    get :rot_free,        on: :collection
+    get :crack_free,      on: :collection
+    get :shrink_free,     on: :collection
+    get :fastenable,      on: :collection
+    get :simple,          on: :collection
+    get :cuttable,        on: :collection
+  end
   root to: 'landing#show'
 end
